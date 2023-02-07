afec4b1ae5f1f484f0096b879c97187d2279bc67
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-02-07 11:17:11 +0000

    chore: tflint fixes

--- api/terraform/main.tf
+++ api/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 4.34.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.3"
+    }
   }
   backend "gcs" {
     # Bucket is passed in via cli arg. Eg, terraform init -reconfigure -backend-configuration=dev.tfbackend
